a836a3a29bc21bbeab5cfcb21f6346509e8f47a7
lautarovinales <lautarovinales14@gmail.com>
2023-07-11 23:53:03 -0300

    commit de los html (vacios)

--- app.js
+++ app.js
@@ -14,6 +14,26 @@ app.get("/",(req,res)=>{
     res.sendFile(ruta);
 })
 
+app.get("/login",(req,res)=>{
+    let ruta = path.join(__dirname, "./views/login.html")
+    res.sendFile(ruta);
+})
+
+app.get("/productCart",(req,res)=>{
+    let ruta = path.join(__dirname, "./views/productCart.html")
+    res.sendFile(ruta);
+})
+
+app.get("/productDetail",(req,res)=>{
+    let ruta = path.join(__dirname, "./views/productDetail.html")
+    res.sendFile(ruta);
+})
+
+app.get("/register",(req,res)=>{
+    let ruta = path.join(__dirname, "./views/register.html")
+    res.sendFile(ruta);
+})
+
 app.listen(puerto, () => {
     console.log('Aplicación esuchando en puerto 3000');
 });
