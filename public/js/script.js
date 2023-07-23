69e3eba00566ef9ed59b4dbf5f67ed289b2a473c
Bishor <84746849+Peteribi73@users.noreply.github.com>
2023-07-22 21:52:30 -0300

    hecho responsive del index para mobile, modifiqué algo de los estilos

---
+++ public/js/script.js
@@ -0,0 +1,120 @@
+function prueba(){
+    console.log("prueba");
+}
+prueba();
+const menuElement = document.querySelector('.menuIcon');
+console.log("asdasdad");
+console.log("prueba");
+console.log("prueba2sssghkhjk");
+
+// @media (min-width: 280px) {
+//     .header-nav{
+//         display: none;
+//     }
+
+//     .productsSection {
+//         display: flex;
+//         justify-content: space-around;
+//         align-items: center;
+//         flex-wrap: wrap;
+//     }
+
+//     .menuIcon{
+//         display: block;
+//     }
+
+//     .title{
+//         font-size: 1.5rem;
+//     }
+
+//     .producto {
+//         width: calc(50% - 20px);
+//         /* max-width: 150px; */
+//         /* height: calc(50% - 20px); */
+//         /* height: 300px; */
+//         /* height: fit-content; */
+//         background-color: red;
+//     }
+
+//     .img-shoe{
+//         /* height: calc(80% - 150px); */
+//         /* height: 100%; */
+//         width: fit-content;
+//         /* height: 150px; */
+//         /* width: 150px; */
+//     }
+
+//     .desc-shoe{
+//         /* height: 100px; */
+//     }
+
+//     .envio{
+//         width: 60px;
+//         height: 20px;
+//         font-size: 0.6em;
+//     }
+
+//     .envio-fav{
+//         display: flex;
+//         justify-content: space-between;
+//         align-items: baseline;
+//     }
+
+//     .envio-fav button{
+//         width: 20px;
+//         height: 20px;
+//         font-size: 0.8em;
+//     }
+
+//     .name-shoe{
+//         font-size: 0.8em;
+//     }
+
+//     .class-shoe{
+//         font-size: 0.5em;
+//     }
+
+//     .disc-price{
+//         display: flex;
+//         justify-content: space-between;
+//         align-items: end;
+//     }
+
+//     .disc-shoe{
+//         font-size: 0.8em;
+//     }
+
+//     .price-shoe{
+//         font-size: 1em;
+//     }
+
+//     .cont-1, .cont-2{
+//         width: 100%;
+//         height: 250px;
+//     }
+
+//     .title-fy{
+//         font-size: 2em;
+//     }
+
+//     .icon{
+//         font-size: 2em;
+//     }
+
+//     .contf{
+//         height: 300px;
+//     }
+
+//     .txt-g{
+//         font-size: 0.8em;
+//     }
+
+//     footer{
+//         height: 450px;
+//     }
+
+//     .footer-nav ul{
+//         flex-direction: column;
+//         text-align: center;
+//     }
+// }
--- script.js
+++
@@ -1,8 +0,0 @@
-function prueba(){
-    console.log("prueba");
-}
-prueba();
-const menuElement = document.querySelector('.menuIcon');
-console.log("asdasdad");
-console.log("prueba");
-console.log("prueba2");
