88137f30acd616f5d424e2c5a487c3a0abf766e1
Bishor <84746849+Peteribi73@users.noreply.github.com>
2023-10-16 17:33:01 -0300

    CRUD de productos hecho

--- controllers/productController.js
+++ controllers/productController.js
@@ -28,7 +28,7 @@ const productController = {
     cart: (req, res) => {
         res.render('./product/productCart', { data: dataCart.results });
     },
-    
+
     addToCart: async (req, res) => {
         const cart = dataCart.results;
         const { results } = dataBase;
@@ -45,7 +45,7 @@ const productController = {
     createProduct: [
         upload.single('img'), // Middleware para cargar una imagen
         async (req, res) => {
-            const { name, price, discount, description, class: clase, sex} = req.body;
+            const { name, price, discount, description, class: clase, sex } = req.body;
             const productImage = req.file ? `/images/products/${req.file.filename}` : '';
             db.Product.create({
                 name,
@@ -66,112 +66,69 @@ const productController = {
     showEditById: (req, res) => {
         let producto = '';
         db.Product.findByPk(req.params.id)
-                .then((result) => {
-                    producto = result;
-                    res.render('./product/productEditById', { producto });
-                })
+            .then((result) => {
+                producto = result;
+                res.render('./product/productEditById', { producto });
+            })
     },
 
     editById: [
         upload.single('img'), // Middleware para cargar una imagen
         async (req, res) => {
-            const { name, price, discount, description, class: clase, sex} = req.body;
+            const { name, price, discount, description, class: clase, sex } = req.body;
             const productImage = req.file ? `/images/products/${req.file.filename}` : '';
 
             db.Product.findByPk(req.params.id)
                 .then((result) => {
                     const productToUpdate = result;
+
+                    name ? productToUpdate.name = name : productToUpdate.name;
+                    price ? productToUpdate.price = price : productToUpdate.price;
+                    discount ? productToUpdate.discount = discount : productToUpdate.discount;
+                    description ? productToUpdate.description = description : productToUpdate.description;
+                    productImage ? productToUpdate.img = productImage : productToUpdate.img;
+                    clase ? productToUpdate.class = clase : productToUpdate.class;
+                    sex ? productToUpdate.sex = sex : productToUpdate.sex;
+
+                    db.Product.update({
+                        name,
+                        price,
+                        discount,
+                        description,
+                        img: productImage || '',
+                        class: clase,
+                        sex
+                    }, {
+                        where: { id: req.params.id }
+                    }).then((result) => {
+                        res.redirect('/');
+                    })
                 })
-
-            name ? productToUpdate.name = name : productToUpdate.name;
-            price ? productToUpdate.price = price : productToUpdate.price;
-            discount ? productToUpdate.discount = discount : productToUpdate.discount;
-            description ? productToUpdate.description = description : productToUpdate.description;
-            productImage ? productToUpdate.img = productImage : productToUpdate.img;
-            clase ? productToUpdate.class = clase : productToUpdate.class;
-            sex ? productToUpdate.sex = sex : productToUpdate.sex;
-
-            db.Product.Update({
-                name,
-                price,
-                discount,
-                description,
-                img: productImage || '',
-                class: clase,
-                sex
-            })
-                .then((result) => {
-                    res.redirect('/');
-                })
-
-
-            // const { title, class: clase, price, discount, img, history } = req.body;
-            // const id = req.params.id;
-
-            // const productImage = req.file;
-            // const imagePath = productImage ? `/images/products/${productImage.filename}` : '';
-
-            // const { results } = dataBase;
-
-            // // Encuentra el producto por su ID
-            // const foundById = results.find((pro) => pro.id === id);
-
-            // // Actualiza los campos del producto con los nuevos valores si están presentes en la solicitud
-            // title ? foundById.title = title : foundById.title;
-            // clase ? foundById.class = clase : foundById.class;
-            // price ? foundById.price = price : foundById.price;
-            // discount ? foundById.discount = discount : foundById.discount;
-            // history ? foundById.history = history : foundById.history;
-            // imagePath ? foundById.img = imagePath : foundById.img;
-
-            // // Filtra los productos para eliminar el producto original y agrega el producto editado
-            // // const productos = results.filter(pro => pro.id != foundById.id);
-            // // productos.push(foundById);
-
-            // // Ordena los objetos en base a su ID
-            // // productos.sort((a, b) => a.id > b.id ? 1 : -1);
-
-            // // Escribe los cambios en el archivo JSON
-            // fs.writeFileSync(path.join(__dirname, '../dataBase/productList.json'), JSON.stringify(dataBase, null, 4));
-
-            // res.redirect('/'); // Redirige después de editar el producto
         }
     ],
 
     catalogo: (req, res) => {
         db.Product.findAll()
             .then((results) => {
-                res.render('./product/productCatalogue', {results})
+                res.render('./product/productCatalogue', { results })
             })
     },
 
     productDetail: (req, res) => {
         db.Product.findByPk(req.params.id)
             .then((result) => {
-                res.render('./product/product', {product: result})
+                res.render('./product/product', { product: result })
             })
     },
 
     productDelete: (req, res) => {
-        const id = req.params.id; // Obtén el ID del parámetro de la solicitud
-
-        // Encuentra el índice del producto a eliminar en el array 'results'
-        const productIndex = dataBase.results.findIndex(product => product.id === id);
-
-        if (productIndex !== -1) {
-            // Elimina el producto del array
-            dataBase.results.splice(productIndex, 1);
-
-            // Ruta al archivo JSON de la base de datos
-            const dbFilePath = path.join(__dirname, '../dataBase/productList.json');
-
-            // Escribe los cambios de vuelta al archivo JSON
-            fs.writeFileSync(dbFilePath, JSON.stringify(dataBase, null, 4));
-        }
-
-        res.redirect('/'); // Redirige a la página principal o a otra página según tu necesidad
+        db.Product.destroy({
+            where: { id: req.params.id }
+        })
+            .then((result) => {
+                res.redirect('/');
+            });
     }
 };
 
-// Exporta el controlador de productos para que pueda ser utilizado en otros archivos
 module.exports = productController;
